6525101d5eb50be47e35df905be657c0561100eb
leeyongjoo <2yongjooo@gmail.com>
2019-01-11 23:59:14 +0900

    succeed crawling

---
+++ crawler.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+import re
+
+url_search = "http://search.danawa.com/dsearch.php?tab=goods&query="
+
+url_vga = url_search + "vga"
+# url_vga += "&page=2"
+
+# use Chrome webdriver
+browser = webdriver.Chrome()
+# wait 3 minutes
+browser.implicitly_wait(3)
+# URL connect
+browser.get(url_vga)
+
+
+# filtering & make csv
+f = open("vga.csv", "w")
+
+products = browser.find_elements_by_css_selector("div[class='main_prodlist main_prodlist_list'] .prod_main_info")
+
+
+for product in products:
+    if product == "":
+        continue
+    f.write(product.find_element_by_css_selector(".prod_name a").text)
+
+    specs = product.find_element_by_css_selector(".prod_spec_set dd").text
+    specs = specs.split(" / ")
+    for spec in specs:
+        s = re.findall("\d+",spec)
+        if s == "":
+            continue
+        else:
+            f.write(",")
+            f.write(s)
+    f.write("\n")
+
+    # print("- name: ", product.find_element_by_css_selector(".prod_name a").text)
+    # print("- spec: ", end="")
+    # specs = product.find_element_by_css_selector(".prod_spec_set dd").text
+    # specs = specs.split(" / ")
+    # for spec in specs:
+    #     s = re.findall("[a-zA-Z]+", spec)
+    #     d = re.findall("[0-9]+", spec)
+    #
+    #     if not len(s) or not len(d):
+    #         continue
+    #     else:
+    #         print(s[0], end="")
+    #         print(d, end="")
+    #         print(",", end="")
+    # print("")
+
+f.close()
+# save...
+# browser.save_screenshot("Website.png")
+
+# close browser
+browser.quit()
