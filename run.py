cfe3e40f9a9e0dd6e011d5348665412dd9a5a370
LeeYongJoo <2yongjooo@gamil.com>
2019-04-30 03:00:51 +0900

    move time.py

--- run.py
+++ run.py
@@ -42,6 +42,41 @@ def doCrawlingDataAndSaveFile(components, dnw, file):
             file.saveListToCsv(productDataList, dirPath, fileName)
     crl.quit()
 
+def doCrawlingDataAndSaveFileUseThread(components, dnw, file):
+    crl = crawler.Crawler() # 크롤러 생성(크롬창 띄우기)
+
+    for comp in components:
+        # 키워드 이름
+        keyword = comp.__class__.__name__.lower()
+
+        # 파일경로, 파일 이름
+        dirs = ["csv"] + [keyword]
+        dirPath = file.generateDirPath(dirs)
+        fileName = file.generateFile(dirPath, keyword, etc.time.getYMD_HM(), 'csv')
+
+        # cpu 는 2 page 까지만...(200개의 상품을 넘어가면 가격이 있는 상품이 얼마 없음)
+        if comp is cpu.Cpu.instance():
+            numpageforSearch = 2
+        else:
+            numpageforSearch = int(numProductforSearch / int(dnw.limit))
+
+        for pageNum in range(1, numpageforSearch+1):
+            # print progressBar
+            etc.progressBar.printProgress(pageNum, numpageforSearch, keyword, 'Complete', 1, 50)
+
+            # connecting url
+            url = dnw.generateUrl(keyword, pageNum)
+
+            # parsing data
+            products = crl.parseElementsByCssSelector(url, dnw.productsSelector)
+
+            # classfying data
+            productDataList = dnw.classfyComponent(keyword, comp, products)
+
+            # saving data
+            file.saveListToCsv(productDataList, dirPath, fileName)
+    crl.quit()
+
 def main():
     # 객체 생성
     dnw = danawa.Danawa()
