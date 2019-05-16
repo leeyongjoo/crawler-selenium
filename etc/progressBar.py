509e0bc2ce40d7e46f3018e4398bf2e8944ef14e
LeeYongJoo <2yongjooo@gamil.com>
2019-05-16 16:35:30 +0900

    sys.stdout.flush() 주석처리

--- etc/progressBar.py
+++ etc/progressBar.py
@@ -8,7 +8,7 @@ def printProgress (iteration, total, prefix = '', suffix = '', decimals = 1, bar
     sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
     if iteration == total:
         sys.stdout.write('\n')
-    sys.stdout.flush()
+    # sys.stdout.flush()
 
 if __name__ == "__main__":
     for i in range(0, 100000):
