18dd645021edf03ba2b2821b4d2c0e720bb81935
LeeYongJoo <2yongjooo@gamil.com>
2019-05-16 16:35:04 +0900

    멀티 프로세스 코드 정상 동작

--- run.py
+++ run.py
@@ -43,12 +43,44 @@ def doCrawlingDataAndSaveFile(components, dnw, file):
             file.saveListToCsv(productDataList, dirPath, fileName)
     crl.quit()
 
+def doCrawlingDataAndSaveFileOneComp(comp, dnw, file):
+    crl = crawler.Crawler() # 크롤러 생성(크롬창 띄우기)
 
+    # 키워드 이름
+    keyword = comp.__class__.__name__.lower()
+
+    # 파일경로, 파일 이름
+    dirs = ["csv"] + [keyword]
+    dirPath = file.generateDirPath(dirs)
+    fileName = file.generateFile(dirPath, keyword, etc.time.getYMD_HM(), 'csv')
+
+    # cpu 는 2 page 까지만...(200개의 상품을 넘어가면 가격이 있는 상품이 얼마 없음)
+    if comp is cpu.Cpu.instance():
+        numpageforSearch = 2
+    else:
+        numpageforSearch = int(numProductforSearch / int(dnw.limit))
+
+    for pageNum in range(1, numpageforSearch+1):
+        # print progressBar
+        etc.progressBar.printProgress(pageNum, numpageforSearch, keyword, 'Complete', 1, 50)
+
+        # connecting url
+        url = dnw.generateUrl(keyword, pageNum)
+
+        # parsing data
+        products = crl.parseElementsByCssSelector(url, dnw.productsSelector)
+
+        # classfying data
+        productDataList = dnw.classfyComponent(keyword, comp, products)
+
+        # saving data
+        file.saveListToCsv(productDataList, dirPath, fileName)
+    crl.quit()
 
 def main():
     # 객체 생성
     dnw = danawa.Danawa()
-    file = sample.file.File()
+    f = sample.file.File()
 
     # 각 부품 객체 생성
     components = []
@@ -62,12 +94,11 @@ def main():
     processes = []
     for comp in components:
         # 크롤링, 파일 저장
-        p = multiprocessing.Process(target=doCrawlingDataAndSaveFile, args=(comp, dnw, file))
+        p = multiprocessing.Process(target=doCrawlingDataAndSaveFileOneComp, args=(comp, dnw, f))
         processes.append(p)
         p.start()
     for p in processes:
         p.join()
-
 
 if __name__ == "__main__":
     main()
